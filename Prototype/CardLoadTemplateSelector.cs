9c56a7edc953850d64fd4c7a35acb262c51b5db1
nealterrell <neal.terrell@csulb.edu>
2015-01-31 16:51:36 -0800

    Cleanup and documentation.

--- Prototype/CardLoadTemplateSelector.cs
+++ Prototype/CardLoadTemplateSelector.cs
@@ -6,6 +6,10 @@ using System.Windows.Controls;
 using System.Windows;
 
 namespace NetrunnerOBS {
+	/// <summary>
+	/// Enables the AutoCompleteTextBox to show the "Loading..." message while it
+	/// filters the card list with the user's query text.
+	/// </summary>
 	public class CardLoadTemplateSelector : DataTemplateSelector {
 		public override DataTemplate SelectTemplate(object item, DependencyObject container) {
 			Window wnd = Application.Current.MainWindow;
--- Prototype/CardViewModel.cs
+++ Prototype/CardViewModel.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ComponentModel;
 using System.Collections;
-using System.Web;
-using System.Xml;
-using System.Net;
-using System.Diagnostics;
-using System.IO;
-using Newtonsoft.Json;
-using System.Xml.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Windows;
+using System.Xml.Linq;
 
 namespace NetrunnerOBS {
 	/// <summary>
